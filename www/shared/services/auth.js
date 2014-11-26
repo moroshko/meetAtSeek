75c6ca8f57c73e4a60c42e0575d09a5aa76ba8cb
Misha Moroshko <michael.moroshko@gmail.com>
2014-11-27 09:56:00 +1100

    Add auth reset

--- www/shared/services/auth.js
+++ www/shared/services/auth.js
@@ -1,4 +1,8 @@
 angular.module('shared').service('Auth', function(storage) {
+  this.reset = function() {
+    storage.set('username', null);
+  };
+
   this.login = function(username) {
     storage.set('username', username);
   };
